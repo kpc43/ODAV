bd3d997ee617fd7a3e5ce76f3c6d4362350f0942
WillCarr242 <156276982+WillCarr242@users.noreply.github.com>
2026-03-30 16:09:18 -0400

    Add files via upload

--- tof_sensor_test_mult_sensors.py
+++ tof_sensor_test_mult_sensors.py
@@ -1,29 +1,34 @@
 import time
-import board
-import busio
-import adafruit_vl53l0x
+import board # type: ignore
+import busio # type: ignore
+import adafruit_vl53l0x # type: ignore
 from gpiozero import OutputDevice
 
-xshut1 = OutputDevice(17, initial_value=False)
-xshut2 = OutputDevice(27, initial_value=False)
-
+xshut1 = OutputDevice(4, initial_value=False)
+xshut2 = OutputDevice(5, initial_value=False)
+xshut3 = OutputDevice(6, initial_value=False)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
 xshut1.on()
 time.sleep(0.1)
 vl531 = adafruit_vl53l0x.VL53L0X(i2c)
-vl531.set_address(0x30)
+vl531.set_address(0x27)
 
 xshut2.on()
 time.sleep(0.1)
 vl532 = adafruit_vl53l0x.VL53L0X(i2c)
+vl532.set_address(0x28)
 
+xshut3.on()
+time.sleep(0.1)
+vl533 = adafruit_vl53l0x.VL53L0X(i2c)
 
 
 
 while True:
     print(f"Sensor1: {vl531.range:.2f}mm")
     print(f"Sensor2: {vl532.range:.2f}mm")
+    print(f"Sensor3: {vl533.range:.2f}mm")
 
     time.sleep(1.0)
